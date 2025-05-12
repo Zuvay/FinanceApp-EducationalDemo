c8f8be4a5e9c2261584b9b773c595e20f11f59c9
Ömer Faruk Yavuz <farukyavuz2025@gmail.com>
2025-05-13 01:04:20 +0300

    Initial commit

---
+++ Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Description { get; set; } = string.Empty;
+    [Required]
+    [Range(0, double.MaxValue, ErrorMessage = "Please enter a valid amount.")]
+    public double Amount { get; set; }
+    [Required]
+    public string Category { get; set; } = string.Empty;
+    public DateTime Date { get; set; } = DateTime.Now;
+}
